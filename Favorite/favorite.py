c917c1882ba82863f8c4c5c401f9162fc95617c3
NateBarnaik <nbarnaiks@gmail.com>
2023-12-01 22:09:20 +0000

    updated favorite.py

--- Favorite/favorite.py
+++ Favorite/favorite.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.errors import OperationFailure
 
 uri = "mongodb+srv://finalproject.d5uengt.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
 client = MongoClient(uri,
                      tls=True,
-                     tlsCertificateKeyFile='../X509-cert-6268247238609712663.pem',
+                     tlsCertificateKeyFile='../FinalProject.pem',
                      server_api=ServerApi('1'))
 
 db = client['favoriteMoviesDATA']
