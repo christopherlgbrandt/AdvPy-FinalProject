d1e675db2825a2f0456d83ca07893fd6cf5fa628
Rhianon Hoisington <lightishredgummi@gmail.com>
2023-12-05 00:01:17 +0000

    created test file

--- FinalApp.py
+++ FinalApp.py
@@ -2,7 +2,7 @@ import requests
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo.errors import OperationFailure
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, render_template, request, redirect, url_for
 # Dependencies: requests, pymongo, flask
 
 app = Flask(__name__)
@@ -27,13 +27,16 @@ collection = db['favoriteMoviesCOLLECT']
 # the comment below can be used for testing to make sure your connected to the database.
 # doc_count = collection.count_documents({})
 
-# print(doc_count) # Should print 1 since their is one document in this collection
+# print(doc_count) # Should print 1 since their is one document in this
+# collection
+
 
 @app.route('/')
 def index():
     # Retrieve data from MongoDB
     data = list(collection.find())
     return render_template('index.html', data=data)
+
 
 @app.route('/add', methods=['POST'])
 def add_task():
@@ -44,12 +47,13 @@ def add_task():
     collection.insert_one(new_data)
     return redirect(url_for('index'))
 
+
 @app.route('/search_movie')
 def search_movie():
     # Example: Searching movies by a keyword
     search_query = request.args.get('query')
     tmdb_endpoint = f'https://api.themoviedb.org/3/search/movie?api_key={tmdb_api_key}&query={search_query}'
-    
+
     # Make a request to TMDB API
     response = requests.get(tmdb_endpoint)
     if response.status_code == 200:
@@ -57,7 +61,8 @@ def search_movie():
         return render_template('movies.html', movies=movies)
     else:
         return "Failed to fetch data from TMDB"
-    
+
+
 @app.route('/movie_info')
 def movie_info():
     movie_id = request.args.get('movie_id')
@@ -69,20 +74,30 @@ def movie_info():
         return render_template('info.html', movie=movie)
     else:
         return "failed to fetch data from TMDB"
-    
+
+
 def favorite(moviePoster, name, releaseDate, overview, reviewScore) -> None:
-   """Allows one to "favorite" a movie saving
-      it to a database on MongoDB
-   """
-   favMovie = {"Movie Poster": moviePoster, "Name": name, "Release Date": releaseDate,
-               "Overview": overview, "Review Score": reviewScore}
-   collection.insert_one(favMovie)
+    """Allows one to "favorite" a movie saving
+       it to a database on MongoDB
+    """
+    favMovie = {
+        "Movie Poster": moviePoster,
+        "Name": name,
+        "Release Date": releaseDate,
+        "Overview": overview,
+        "Review Score": reviewScore}
+    collection.insert_one(favMovie)
+
 
 def unFavorite(moviePoster, name, releaseDate, overview, reviewScore) -> None:
-   """Unfavorites a movie
-   """
-   collection.delete_one({"Movie Poster": moviePoster, "Name": name, "Release Date": releaseDate,
-                         "Overview": overview, "Review Score": reviewScore})
+    """Unfavorites a movie
+    """
+    collection.delete_one({"Movie Poster": moviePoster,
+                           "Name": name,
+                           "Release Date": releaseDate,
+                           "Overview": overview,
+                           "Review Score": reviewScore})
+
 
 @app.route('/add_favorite', methods=['POST'])
 def add_favorite():
@@ -98,6 +113,8 @@ def add_favorite():
     return "Movie added to favorites successfully!"
 
 # Route for removing a movie from favorites
+
+
 @app.route('/remove_favorite', methods=['POST'])
 def remove_favorite():
     # Get movie details from the request
@@ -111,6 +128,7 @@ def remove_favorite():
     )
     return "Movie removed from favorites successfully!"
 
+
 @app.route('/favorites')
 def print_favorite():
     """Prints the list of favorite movies from MongoDB"""
@@ -120,6 +138,7 @@ def print_favorite():
         return render_template('favorites.html', movies=movies)
     else:
         return "No results found in favorites"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
